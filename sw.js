13d5fd6ca592b4df931f6549afa282b7e5804bc2
Will <30760648+WillDera@users.noreply.github.com>
2018-11-29 22:31:40 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+var shellCacheName = "pwa-todo-v1";
+var filesToCache = [
+  "./",
+  "./index.html",
+  "./content/css/screen.min.css",
+  "./content/js/app.js",
+  "https://cdn.jsdelivr.net/npm/vue",
+  "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
+];
